efa307695b19021dc23bf234dd4e51f8957b5844
TheBlaqHulk <47624941+TheBlaqHulk@users.noreply.github.com>
2019-02-14 18:28:54 +0200

    Add files via upload

---
+++ challengesPage/main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+   
+
+
+
+  })
+
+
+
+
+function check65() {
+    var k = parseInt(document.getElementById("check65_1").value);
+    var q = parseInt(document.getElementById("check65_2").value);
+    console.log(k + " " + q);
+	if (k === 65 || q === 65 || k + q === 65) {
+        console.log(true);
+        document.getElementById("check65_answer").innerHTML = "True";
+	} else {
+        console.log(false);
+        document.getElementById("check65_answer").innerHTML = "False";
+	}
+}
+
+
+function triarea() {
+    var a = parseInt(document.getElementById("triarea_1").value);
+    var b = parseInt(document.getElementById("triarea_2").value);
+    var c = parseInt(document.getElementById("triarea_3").value);
+    peri = (a + b +c) / 2;
+    area = Math.sqrt((peri *(peri - a)*(peri - b)*(peri - c)));
+    document.getElementById("triarea_answer").innerHTML = area;
+  //  return area;
+  console.log(area);
+} 
